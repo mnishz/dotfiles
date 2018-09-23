6ab72997e433411cd78a968342d15ea450660a16
mnishz <mnishz@users.noreply.github.com>
2018-09-23 15:47:04 +0900

    add a shell script to make symbolic links

---
+++ MakeSymbolicLink.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+# ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
+ln -sf ~/dotfiles/.dein.toml ~/.dein.toml
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/vimfiles ~/.vim
