53b8481bab609118552218f1217f1584d8dc02fb
mnishz <mnishz@users.noreply.github.com>
2019-01-13 03:59:56 +0000

    add .gdbinit

--- MakeSymbolicLink.sh
+++ MakeSymbolicLink.sh
@@ -3,4 +3,5 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 # ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/.dein.toml ~/.dein.toml
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.gdbinit ~/.gdbinit
 ln -sf ~/dotfiles/vimfiles ~/.vim
