c4100985a8a52eaf4da1c9720d2380b5483bd95d
mnishz <mnishz@users.noreply.github.com>
2019-02-11 12:42:48 +0900

    bash

--- make_symbolic_link.sh
+++ make_symbolic_link.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 # ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/.dein.toml ~/.dein.toml
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gdbinit ~/.gdbinit
 ln -sf ~/dotfiles/.bashrc.additional ~/.bashrc.additional
+ln -sf ~/dotfiles/.git_prompt.sh ~/.git_prompt.sh
+rm ~/.vim
 ln -sf ~/dotfiles/vimfiles ~/.vim
