b07dca95db3482d6aef1db64dc973ca69e55e827
mnishz <mnishz@users.noreply.github.com>
2019-05-28 20:35:11 +0900

    add screenrc to symbolic link file

--- make_symbolic_link.sh
+++ make_symbolic_link.sh
@@ -4,6 +4,7 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.dein.toml ~/.dein.toml
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gdbinit ~/.gdbinit
+ln -sf ~/dotfiles/.screenrc ~/.screenrc
 ln -sf ~/dotfiles/.bashrc.additional ~/.bashrc.additional
 ln -sf ~/dotfiles/.git_prompt.sh ~/.git_prompt.sh
 rm ~/.vim
