21cc2d62f7f146c3e7886e9b0007edd9d78b8c46
mnishz <mnishz@users.noreply.github.com>
2019-07-08 11:52:57 +0900

    add symbolic link batch for MSYS2

---
+++ MakeSymbolicLink_MSYS2.bat
@@ -0,0 +1,8 @@
+mklink %~dp0..\.vimrc %~dp0.vimrc
+mklink %~dp0..\.gvimrc %~dp0.gvimrc
+mklink %~dp0..\.dein.toml %~dp0.dein.toml
+mklink %~dp0..\.gitconfig %~dp0.gitconfig
+mklink %~dp0..\.gdbinit %~dp0.gdbinit
+mklink %~dp0..\.screenrc %~dp0.screenrc
+mklink %~dp0..\.bashrc.additional %~dp0.bashrc.additional
+mklink /d %~dp0..\.vim %~dp0vimfiles
