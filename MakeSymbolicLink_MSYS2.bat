e42d768d1d00d54f6b2270f59230a345af32038c
mnishz <mnishz@users.noreply.github.com>
2019-11-20 15:34:21 +0900

    tigrc

--- MakeSymbolicLink_MSYS2.bat
+++ MakeSymbolicLink_MSYS2.bat
@@ -6,6 +6,7 @@ rem mklink %~dp0..\.gdbinit %~dp0.gdbinit
 rem mklink %~dp0..\.screenrc %~dp0.screenrc
 rem mklink %~dp0..\.tmux.conf %~dp0.tmux.conf
 mklink %~dp0..\.minttyrc %~dp0.minttyrc
+mklink %~dp0..\.tigrc %~dp0.tigrc
 mklink %~dp0..\.bashrc.additional %~dp0.bashrc.additional
 rem mklink %~dp0..\.git_prompt.sh %~dp0.git_prompt.sh
 mklink %~dp0..\git_clean.sh %~dp0git_clean.sh
--- make_symbolic_link.sh
+++ make_symbolic_link.sh
@@ -7,6 +7,7 @@ ln -sf ~/dotfiles/.gdbinit ~/.gdbinit
 ln -sf ~/dotfiles/.screenrc ~/.screenrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 # ln -sf ~/dotfiles/.minttyrc ~/.minttyrc
+ln -sf ~/dotfiles/.tigrc ~/.tigrc
 ln -sf ~/dotfiles/.bashrc.additional ~/.bashrc.additional
 # ln -sf ~/dotfiles/.git_prompt.sh ~/.git_prompt.sh
 ln -sf ~/dotfiles/git_clean.sh ~/git_clean.sh
