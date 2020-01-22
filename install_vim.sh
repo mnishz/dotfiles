30a94d354443852ea85458d768c4a85c68dcd1d0
mnishz <mnishz@users.noreply.github.com>
2020-01-23 08:26:42 +0900

    improve install_vim.sh

--- install_vim.sh
+++ install_vim.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 set -eu
 
-sudo apt install git gcc make gettext libtinfo-dev libacl1-dev libgpm-dev libtool-bin
-# for Python
-# sudo apt install python3-dev
+# sudo apt install git gcc make gettext libtinfo-dev libacl1-dev libgpm-dev libtool-bin python3-dev curl
+sudo apt install git gcc make libtinfo-dev libtool-bin python3-dev curl
 # for GUI
 # sudo apt install libxmu-dev libgtk-3-dev libxpm-dev
 
 cd ~
 git clone https://github.com/vim/vim
 cd vim/src
-./configure --prefix=${HOME} --with-features=huge --enable-fail-if-missing
-# for Python
-# ./configure --prefix=${HOME} --with-features=huge --enable-fail-if-missing --enable-python3interp
+./configure --prefix=${HOME} --with-features=huge --enable-fail-if-missing --enable-python3interp
 # for GUI
 # ./configure --prefix=${HOME} --with-features=huge --enable-fail-if-missing --enable-python3interp --enable-gui=gtk3 --enable-autoservername
 make -j
