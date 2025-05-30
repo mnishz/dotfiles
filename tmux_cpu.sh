cad1b50db43925da4cbdb58d5b66ed3c718b182e
mnishz <mnishz@users.noreply.github.com>
2025-05-30 14:11:19 +0900

    add % to tmux_cpu.sh

--- tmux_cpu.sh
+++ tmux_cpu.sh
@@ -5,7 +5,7 @@ cpu_usage=$(top -b -n 1 | grep "%Cpu" | awk '{print $2 + $4}')
 cpu_usage_int=$(echo "$cpu_usage" | awk '{print int($1)}')
 cpu_usage=$(printf "%.1f" $cpu_usage)
 if (( $cpu_usage_int >= 80 )); then
-    echo "#[bg=yellow]${cpu_usage}#[default]"
+    echo "#[bg=yellow]${cpu_usage}%#[default]"
 else
-    echo "${cpu_usage}"
+    echo "${cpu_usage}%"
 fi
